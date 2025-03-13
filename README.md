d167998a0561c60b511dd33736a9401b83e72274
winnieepm <71612467+winnieepm@users.noreply.github.com>
2025-03-13 15:26:33 -0400

    trblshooting studio.js

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 this is a test site where i'm following [this tutorial](https://learn-eleventy.pages.dev/) to setup my first 11ty site. if it works i'll integrate lunr to add a search function. once tested, i'll make a site for locally displaying a tweet dataset without distribution. 
 
 ## dev notes
+### 2025-03-13
+- for some reason the `async ()` in studio.js doesn't work. the file is parsed, but apparently the function isn't properly run.
+
 ### 2025-03-04
 - there's clever uses of 'aria' labels in the tutorial that can be added to improve accesibility and UX.
 - the `_data` folder is where i'd put json files with twitter data.
--- src/_data/studio.js
+++ src/_data/studio.js
@@ -1,21 +1,26 @@
-const EleventyFetch = require('@11ty/eleventy-fetch');
+// _data/studio.js
+console.log("Studio data file is being processed!");
 
-/**
- * Grabs the remote data for studio images and returns back
- * an array of objects
- *
- * @returns {Array} Empty or array of objects
- */
+const EleventyFetch = require("@11ty/eleventy-fetch");
+
 module.exports = async () => {
-	try {
-		let url = 'https://11ty-from-scratch-content-feeds.piccalil.li/media.json';
-		const { items } = await EleventyFetch(url, {
-			duration: '1d',
-			type: 'json',
-		});
-		return items;
-	} catch (err) {
-		console.log(err);
-		return [];
-	}
+  try {
+    const url = "https://11ty-from-scratch-content-feeds.piccalil.li/media.json";
+    console.log("Fetching data from:", url); // Debugging: Log the URL
+
+    const response = await EleventyFetch(url, {
+      duration: "1d",
+      type: "json",
+    });
+
+    console.log("API response:", response); // Debugging: Log the full response
+
+    const { items } = response;
+    console.log("Fetched items:", items); // Debugging: Log the items
+
+    return items;
+  } catch (err) {
+    console.error("Error fetching data:", err); // Debugging: Log any errors
+    return [];
+  }
 };
